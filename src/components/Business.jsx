1776e790fa29aae26f2c7b0f1a17643f9a749731
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-03 13:45:12 +1200

    Added Business Component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -12,19 +12,19 @@ const business = {
 
 function Business() {
     return (
-        <div class="flex flex-col w-1/4">
+        <div className="flex flex-col w-1/4">
             <img src={business.image} alt={business.name}/>
-            <h4 class="text-left text-xl font-bold">{business.name}</h4>
-            <div class="flex flex-row">
-                <div class="basis-2/3 text-left">
+            <h4 className="text-left text-xl font-bold">{business.name}</h4>
+            <div className="flex flex-row">
+                <div className="basis-2/3 text-left">
                     <p>{business.address}</p>
                     <p>{business.city}</p>
                     <p>{business.state} {business.zip}</p>
                 </div>
-                <div class="basis-1/3 text-right">
-                    <p class="uppercase text-xl text-yellow-600 font-bold">{business.category}</p>
-                    <p class="text-yellow-600 font-bold">{business.rating} stars</p>
-                    <p class="">{business.reviewCount} reviews</p>
+                <div className="basis-1/3 text-right">
+                    <p className="uppercase text-xl text-yellow-600 font-bold">{business.category}</p>
+                    <p className="text-yellow-600 font-bold">{business.rating} stars</p>
+                    <p className="">{business.reviewCount} reviews</p>
                 </div>
             </div>
         </div>
